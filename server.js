7cf1d1bcaa238bbb8138c72ace8b1b55e8879d89
Paulo Henrique Vieira Candido <pcandido@cpqd.com.br>
2021-09-04 17:04:18 -0300

    Adicionando suporte a multiplos chats em estado temporário

--- server.js
+++ server.js
@@ -6,19 +6,13 @@ const app = express();
 const server = require("http").createServer(app);
 
 app.use(cors({ origin: "*" }));
-const defineHeaders = function (res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setTimeout(120 * 60 * 1000, function () {
-    res.status(408).json("Request has timed out.");
-  });
-};
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
 app.use("/", (req, res) => {
-  defineHeaders();
   res.render("index.html");
 });
 
@@ -30,23 +24,24 @@ const io = require("socket.io")(server, {
 
 let messages = [];
 io.on("connection", (socket) => {
-  console.log("Socket conectado" + socket.id);
   socket.on("join", (room) => {
     socket.join(room);
-    socket.to(room).emit("previousMessages", messages);
+    if (!Boolean(messages[room])) {
+      messages[room] = [];
+    }
+    io.sockets.in(room).emit('previousMessages', messages[room]);
   });
 
-  socket.emit("previousMessages", messages);
-
   socket.on("sendMessage", (data) => {
+    if(!data) return;
     if (!Boolean(messages[data.room])) {
       messages[data.room] = [];
     }
 
     messages[data.room].push(data);
-    console.log(messages);
     socket.to(data.room).emit("recivedMessage", data);
   });
+
 });
 
 server.listen(3000);
